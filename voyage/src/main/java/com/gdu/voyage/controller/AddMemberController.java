4ce9b0ab63cd7c78c8717e59e2fa4377d0b28101
SH <SH@192.168.219.102>
2021-12-09 23:05:13 +0900

    [김상훈] addMemberController 추가

---
+++ voyage/src/main/java/com/gdu/voyage/controller/AddMemberController.java
@@ -0,0 +1,14 @@
+package com.gdu.voyage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AddMemberController {
+	@GetMapping("/addMember")
+	   public String index() {
+	      System.out.println("AddMemberController() 실행");
+	      
+	      return "addMember";
+	   }
+}	
