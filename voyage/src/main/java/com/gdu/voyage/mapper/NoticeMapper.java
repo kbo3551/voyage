0c05a4221959bc97dcc621d94711d563ae170c5c
김보령 <@192.168.219.100>
2021-12-10 17:36:45 +0900

    [김보령]vo_NoticeForm.java 추가 mapper_NoticeMapper.xml 수정 Service_미완성 NoticeService.java추가

--- voyage/src/main/java/com/gdu/voyage/mapper/NoticeMapper.java
+++ voyage/src/main/java/com/gdu/voyage/mapper/NoticeMapper.java
@@ -6,12 +6,12 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.gdu.voyage.vo.Notice;
+import com.gdu.voyage.vo.NoticeFile;
 
 @Mapper
 public interface NoticeMapper {
 	// 리스트 가져오기
 	List<Notice> selectNoticeList(Map<String, Object> param);
-	
 	// 공지사항 작성
 	int inserNotice(Notice notice);
 	// 공지사항 삭제
@@ -20,5 +20,8 @@ public interface NoticeMapper {
 	void updateNotice(Notice notice);
 	// 공지사항 One
 	Notice selectNoticeOne(int noticeNo);
-	
+	// 공지사항 파일
+	List<NoticeFile> selectNoticeFileList();
+	int insertNotice(Notice notice);
+	int insertNoticefile(NoticeFile noticefile);
 }
--- voyage/src/main/java/com/gdu/voyage/service/NoticeService.java
+++ voyage/src/main/java/com/gdu/voyage/service/NoticeService.java
@@ -1,5 +1,50 @@
 package com.gdu.voyage.service;
 
-public class NoticeService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.gdu.voyage.mapper.NoticeMapper;
+import com.gdu.voyage.vo.Notice;
+import com.gdu.voyage.vo.NoticeFile;
+import com.gdu.voyage.vo.NoticeForm;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Transactional
+public class NoticeService {
+	@Autowired NoticeMapper noticeMapper;
+	
+	public List<NoticeFile> getNoticeFileList(){
+		return noticeMapper.selectNoticeFileList();
+	}
+	public void addNotice(NoticeForm noticeForm) {
+		log.debug(noticeForm.toString()+"☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆[보령]NoticeService_noticeform");
+		String noticeContent = noticeForm.getNoticeContent();
+		List<MultipartFile> file = noticeForm.getNoticefile();
+		
+		// 공지사항 입력
+		Notice notice = new Notice();
+		notice.setNoticeContent(noticeContent);
+		noticeMapper.insertNotice(notice);
+		log.debug(notice.getNoticeNo()+"☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆[보령]NoticeService_noticeform");
+
+	// 파일 업로드가 되면
+	if(file != null) {
+		// Noticefile 입력
+		NoticeFile noticefile = new NoticeFile();
+		noticefile.setNoticeNo(notice.getNoticeNo());
+		
+	/*	String originName = file.getOriginalFilename();
+		int p = originName.lastIndexOf(".");
+
+
+	*/
+		}
+	}
 }
