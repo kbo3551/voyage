da899d0d54e08a89dfa7163d2d72616dbeecd6d4
김보령 <@192.168.219.100>
2021-12-10 22:01:15 +0900

    [김보령]NoticeController 수정

--- voyage/src/main/java/com/gdu/voyage/controller/NoticeController.java
+++ voyage/src/main/java/com/gdu/voyage/controller/NoticeController.java
@@ -1,14 +1,47 @@
 package com.gdu.voyage.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.gdu.voyage.service.NoticeService;
+import com.gdu.voyage.vo.NoticeFile;
+import com.gdu.voyage.vo.NoticeForm;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class NoticeController {
+	@Autowired NoticeService noticeService;
+	
 	@GetMapping("/noticeList") // 공지사항 게시판 리스트
 	public String getAccomProduct() {
 		System.out.println("NoticeController() 실행");
 	      
 		return "/templates_citylisting/noticeList";
+	}
+	@GetMapping("/addNotice")
+	public String addNotice() {
+		return "addNotice";
+	}
+	
+	@PostMapping("/addNotice")
+	public String addNotice(NoticeForm noticeForm) {
+		log.debug(noticeForm.toString());
+		noticeService.addNotice(noticeForm);
+		return "redirect:/noticefileList";
+	}
+	
+	@GetMapping("/noticefileList") 
+	public String getAccomProduct(Model fmodel) {
+		System.out.println("NoticeController() 실행");
+		List<NoticeFile> list = noticeService.getNoticeFileList();
+	    fmodel.addAttribute("list",list);
+		return "noticefileList";
 	}
 }
