a6ef06e86bbb691932eb3bcb94c18933b89db085
김보령 <@192.168.219.100>
2021-12-10 11:09:15 +0900

    [김보령]vo_Notice.java추가 mapper_NoticeMapper.java 추가

---
+++ voyage/src/main/java/com/gdu/voyage/mapper/NoticeMapper.java
@@ -0,0 +1,22 @@
+package com.gdu.voyage.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.gdu.voyage.vo.Notice;
+
+@Mapper
+public interface NoticeMapper {
+	// 리스트 가져오기
+	List<Notice> selectNoticeList(Map<String, Object> param);
+	int selectNoticeListCont(Map<String, String> map);
+	// 공지사항 작성
+	int inserNotice(Notice notice);
+	// 공지사항 삭제
+	void deleteNotice(Notice notice);
+	// 공지사항 수정
+	void updateNotice(Notice notice);
+	
+}
