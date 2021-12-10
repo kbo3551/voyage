3b5137e70b612423bae1be2686c167c4ac69d7b5
PYJ <PYJ@DESKTOP-PRQ77L6>
2021-12-10 18:00:07 +0900

    [박예진] banner.jsp 에서 hashtag.jsp 호출 방법 include - java 코드 오류, import - 경로 오류

--- voyage/src/main/java/com/gdu/voyage/mapper/HashtagMapper.java
+++ voyage/src/main/java/com/gdu/voyage/mapper/HashtagMapper.java
@@ -3,9 +3,12 @@ package com.gdu.voyage.mapper;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.gdu.voyage.vo.Hashtag;
 
+@Mapper
 public interface HashtagMapper {
 	// 해시태그 가져오기
-	List<Hashtag> selectHashtagList(Map<String, Object> param);
+	List<Hashtag> selectHashtagList();
 }
--- voyage/src/main/java/com/gdu/voyage/vo/Hashtag.java
+++ voyage/src/main/java/com/gdu/voyage/vo/Hashtag.java
@@ -6,11 +6,8 @@ import lombok.Data;
 public class Hashtag {
 	private int hashtagNo;
 	private int idenNo;
-<<<<<<< HEAD
 	private String table_name;
-=======
 	private String tableName;
->>>>>>> branch 'master' of https://github.com/lumpyspaceyz/voyage.git
 	private String hashtag;
 	private String createDate;
 }
