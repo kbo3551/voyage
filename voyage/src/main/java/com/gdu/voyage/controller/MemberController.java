5957755b8696f57662f68e3de6da9d43faab7f96
yis45 <yis45@DESKTOP-TTQ7F39>
2021-12-10 23:38:02 +0900

    [조유태] 회원가입 기능 구현, 수정

--- voyage/src/main/java/com/gdu/voyage/controller/MemberController.java
+++ voyage/src/main/java/com/gdu/voyage/controller/MemberController.java
@@ -2,15 +2,16 @@ package com.gdu.voyage.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gdu.voyage.service.MemberService;
-import com.gdu.voyage.vo.Address;
 import com.gdu.voyage.vo.Member;
+import com.gdu.voyage.vo.MemberAddress;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,17 +19,17 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional
 @Controller
 public class MemberController {
-	MemberService memberService;
+	@Autowired MemberService memberService;
 	
 	@GetMapping("/addMember")
 	public String getAddMember() {
     	System.out.println("MemberController() 실행");
-      
     	return "addMember";
     }
+
 	
 	@PostMapping("/addMember")
-	public String postAddMember(HttpServletRequest request, Model model) {
+	public String postAddMember(HttpServletRequest request, RedirectAttributes redirect) {
 		System.out.println("MemberController() 실행");
 	    String memberId = request.getParameter("id");
 	    String memberPw = request.getParameter("password");
@@ -38,21 +39,13 @@ public class MemberController {
 	    String memberPhone = request.getParameter("phone");
 	    String memberEmail = request.getParameter("email");
 	    String memberReg = request.getParameter("socialsecuritynumber");
-	    int addressPostalCode = Integer.parseInt(request.getParameter("postalCode"));
-	    String addressZip = request.getParameter("roadAddress");
-	    String addressDetail = request.getParameter("detailAddress");
+	    int memberAddressPostalCode = Integer.parseInt(request.getParameter("postalCode"));
+	    String memberAddressZip = request.getParameter("roadAddress");
+	    String memberAddressDetail = request.getParameter("detailAddress");
 	    String memberDescription = request.getParameter("description");
-	    
-	    
-	    // 주소 객체
-	    Address addr = new Address();
-	    addr.setAddressPostalCode(addressPostalCode);
-	    addr.setAddressZip(addressZip);
-        addr.setAddressDetail(addressDetail);
 	    
 	    // 맴버 객체. 포함되지 않은 4개의 값은 Mapper에 존재
 	    Member m = new Member();
-	    m.setAddressNo(addr.getAddressNo());
 	    m.setMemberId(memberId);
 	    m.setMemberPw(memberPw);
 	    m.setMemberFirstName(memberFirstName);
@@ -62,29 +55,38 @@ public class MemberController {
 	    m.setMemberEmail(memberEmail);
 	    m.setMemberReg(memberReg);
 	    m.setMemberDescription(memberDescription);
+	    m.setMemberLevel(0);
+	    m.setMemberActive("활동");
 	    
+	    // 디버그
+	    log.trace("★controller★"+m.toString());
 	    
 	    // 중복 검사
 	    String duplCheck = memberService.duplMemberCheck(m);
 	    if(duplCheck.equals("아이디중복")) {
-	    	model.addAttribute("member", m);
-	    	return "addMember?duplication=id";
+	    	redirect.addFlashAttribute("member",m);
+	    	return "redirect:addMember?duplication=id";
 	    }
 	    if(duplCheck.equals("닉네임중복")) {
-	    	model.addAttribute("member", m);
-	    	return "addMember?duplication=nickname";
+	    	redirect.addFlashAttribute("member",m);
+	    	return "redirect:addMember?duplication=nickname";
 	    }
-	    
-         
-	    //디버그
-        log.trace("★controller★"+addr.toString());
-        log.trace("★controller★"+m.toString());
-	      
-	    // 주소 삽입. 오토인클리먼트 값을 받아옴
-	    memberService.addMemberAddress(addr);
 	    
 	    // 멤버 추가
 	    memberService.addMember(m);
+	    
+	    // 주소 객체
+	    MemberAddress addr = new MemberAddress();
+	    addr.setMemberId(memberId);
+	    addr.setMemberAddressPostalCode(memberAddressPostalCode);
+	    addr.setMemberAddressZip(memberAddressZip);
+        addr.setMemberAddressDetail(memberAddressDetail);
+        
+        // 디버그
+        log.trace("★controller★"+addr.toString());
+        
+        // 주소 삽입. 오토인클리먼트 값을 받아옴
+	    memberService.addMemberAddress(addr);
 	    
 	    return "login";
 	 }
