32bc02658ebb85f98bc7df86d9d5436b3ee4f90f
yis45 <yis45@DESKTOP-TTQ7F39>
2021-12-10 16:06:54 +0900

    [조유태] MemberMapper 뼈대 제작

---
+++ voyage/src/main/java/com/gdu/voyage/mapper/MemberMapper.java
@@ -0,0 +1,12 @@
+package com.gdu.voyage.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.gdu.voyage.vo.Address;
+import com.gdu.voyage.vo.Member;
+
+@Mapper
+public interface MemberMapper {
+	void insertMemberAddress(Address address);
+	void insertMember(Member mbmMember);
+}
