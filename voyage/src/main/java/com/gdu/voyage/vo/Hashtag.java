532438f8b215bd1ecaf751fcdc887a39b4bda49e
PYJ <PYJ@DESKTOP-PRQ77L6>
2021-12-10 18:02:59 +0900

    [박예진] Hashtag.vo 수정

--- voyage/src/main/java/com/gdu/voyage/vo/Hashtag.java
+++ voyage/src/main/java/com/gdu/voyage/vo/Hashtag.java
@@ -6,10 +6,7 @@ import lombok.Data;
 public class Hashtag {
 	private int hashtagNo;
 	private int idenNo;
-<<<<<<< HEAD
 	private String table_name;
-=======
->>>>>>> branch 'master' of https://github.com/lumpyspaceyz/voyage.git
 	private String tableName;
 	private String hashtag;
 	private String createDate;
