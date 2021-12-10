585c4d5a70450ba0533200d121971ab517f42c8e
김보령 <@192.168.219.100>
2021-12-10 11:19:52 +0900

    [김보령]vo_Notice.java 수정

--- voyage/src/main/java/com/gdu/voyage/vo/Notice.java
+++ voyage/src/main/java/com/gdu/voyage/vo/Notice.java
@@ -5,8 +5,11 @@ import lombok.Data;
 @Data
 public class Notice {
 	private int noticeNo;
+	private String adminId;
 	private String noticeTitle;
 	private String noticeContent;
+	private String noticeTop;
+	private int noticeViewCnt;
 	private String createDate;
 	private String updateDate;
 }
