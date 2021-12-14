768231a88362ca15b8b6a425a97b6ffb666f4224
jihye <samsung@LAPTOP-RVQADFAO>
2021-12-14 17:26:27 +0900

    [최지혜] 숙소_건물의 주소 등록을 위한 vo 최종 수정
    주소 등록을 위한 vo 수정 완료
    추천장소(spot)의 주소를 저장하기 위해 시간이 오래 걸림
    추천장소는 하나의 숙소에 여러개를 추가 할 수 있기 때문에
    vo를 숙소 form vo 안에 추천장소 form vo를 더 만듦

---
+++ voyage/src/main/java/com/gdu/voyage/vo/AccomAddress.java
@@ -0,0 +1,12 @@
+package com.gdu.voyage.vo;
+
+import lombok.Data;
+
+@Data
+public class AccomAddress {
+	private int AccomAddressNo;
+	private int AccomBuildingNo;
+	private int AccomAddressPotalCode;
+	private String AccomAddressZip;
+	private String AccomAddressDetail;
+}
---
+++ voyage/src/main/java/com/gdu/voyage/vo/AccomSpotForm.java
@@ -0,0 +1,9 @@
+package com.gdu.voyage.vo;
+
+import lombok.Data;
+
+@Data
+public class AccomSpotForm {
+	private AccomBuildingSpot accomBuildingSpot;
+	private SpotAddress spotAddress;
+}
