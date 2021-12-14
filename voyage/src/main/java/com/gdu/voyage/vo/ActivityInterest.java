61b2cb06f60cd08a83a149554b65722e6fe048d6
jihye <samsung@LAPTOP-RVQADFAO>
2021-12-14 11:35:56 +0900

    [최지혜] vo 생성 및 수정
    체험 관련 페이지를 개발하기 위해 vo들을 생성하거나 수정함

---
+++ voyage/src/main/java/com/gdu/voyage/vo/ActivityInterest.java
@@ -0,0 +1,10 @@
+package com.gdu.voyage.vo;
+
+import lombok.Data;
+
+@Data
+public class ActivityInterest {
+	private int activityInterestNo;
+	private int activityNo;
+	private String memberId;
+}
