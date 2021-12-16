d99d4b3126eb7363eb1b890b5869643d502cdf2a
PYJ <PYJ@DESKTOP-PRQ77L6>
2021-12-16 12:17:52 +0900

    [박예진] 상품(체험,숙소) 목록 출력을 위한 product contrller, service, mapper 생성

--- voyage/src/main/java/com/gdu/voyage/controller/ProductController.java
+++ voyage/src/main/java/com/gdu/voyage/controller/ProductController.java
@@ -1,14 +1,22 @@
 package com.gdu.voyage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.gdu.voyage.service.ProductService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class ProductController {
+	@Autowired ProductService productService;
+	
 	@GetMapping("/getAccomProductList")
 	public String getAccomProduct() {
 		System.out.println("ProductController() 실행");
-	      
+	    
 		return "/getAccomProductList";
 	}
 	
