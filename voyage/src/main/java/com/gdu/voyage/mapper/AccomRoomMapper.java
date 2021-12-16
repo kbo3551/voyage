442384ba519e808bc888ea3ee3efb802c54ee90f
jihye <samsung@LAPTOP-RVQADFAO>
2021-12-16 15:33:13 +0900

    [최지혜] 숙소_객실 추가를 위한 vo, mapper, mapper 생성 및 수정
    숙소_객실 추가를 위해 파일들 생성
    controller는 다른 팀원분이 만지고 있을거 같아서 숙소_객실로 나눠진 부분만 커밋

--- voyage/src/main/java/com/gdu/voyage/mapper/AccomRoomMapper.java
+++ voyage/src/main/java/com/gdu/voyage/mapper/AccomRoomMapper.java
@@ -4,8 +4,17 @@ import java.util.List;
 import java.util.Map;
 
 import com.gdu.voyage.vo.AccomRoom;
+import com.gdu.voyage.vo.AccomRoomImage;
+import com.gdu.voyage.vo.AccomRoomItem;
+import com.gdu.voyage.vo.Hashtag;
 
 public interface AccomRoomMapper {
 	List<AccomRoom> selectAccomRoomList(Map<String, Object> param);
 	
+	// 사업자
+	// 숙소-객실 입력 : 숙소-객실정보 + 이미지 + 시설 + 추천장소 + 물품옵션
+	int insertAccomRoom(AccomRoom accomRoom);
+	int insertAccomRoomImage(AccomRoomImage accomRoomImage);
+	int insertAccomRoomItem(AccomRoomItem accomRoomItem);
+	int insertAccomRoomHashtag(Hashtag Hashtag);
 }
---
+++ voyage/src/main/java/com/gdu/voyage/vo/AccomRoomItem.java
@@ -0,0 +1,12 @@
+package com.gdu.voyage.vo;
+
+import lombok.Data;
+
+@ Data
+public class AccomRoomItem {
+	private int accomRoomItemNo;
+	private int accomRoomNo;
+	private String accomRoomItemName;
+	private int accomRoomItemQnt;
+	private int accomRoomItemPrice;
+}
