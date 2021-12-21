33dba292b66b349b9efac39331d2510e83147c84
jihye <samsung@LAPTOP-RVQADFAO>
2021-12-21 15:53:19 +0900

    [최지혜] Hashtag vo 수정
    table_name이 두 개라서 하나로 변경

--- voyage/src/main/java/com/gdu/voyage/vo/Hashtag.java
+++ voyage/src/main/java/com/gdu/voyage/vo/Hashtag.java
@@ -6,7 +6,6 @@ import lombok.Data;
 public class Hashtag {
 	private int hashtagNo;
 	private int idenNo;
-	private String table_name;
 	private String tableName;
 	private String hashtag;
 	private String createDate;
