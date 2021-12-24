f9ceeb7ae72ab460996f94f8d06ad7ed22706a8e
yis45 <yis45@DESKTOP-903OS3K>
2021-12-24 13:34:59 +0900

    [조유태] accomPaymentMapper+accomPaymentService

--- voyage/src/main/java/com/gdu/voyage/mapper/AccomPaymentMapper.java
+++ voyage/src/main/java/com/gdu/voyage/mapper/AccomPaymentMapper.java
@@ -9,5 +9,9 @@ import com.gdu.voyage.vo.AccomPayment;
 
 @Mapper
 public interface AccomPaymentMapper {
+	// 숙소 결제 목록
 	List<AccomPayment> selectAccomPayment(Map<String, Object> map);
+	// 페이징을 위한 갯수
+	int selectCountPage(String memberId);
+	
 }
---
+++ voyage/src/main/java/com/gdu/voyage/service/AccomPaymentService.java
@@ -0,0 +1,51 @@
+package com.gdu.voyage.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gdu.voyage.mapper.AccomPaymentMapper;
+import com.gdu.voyage.vo.AccomPayment;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Transactional
+public class AccomPaymentService {
+	@Autowired private AccomPaymentMapper accomPaymentMapper;
+	
+	public Map<String, Object> selectAccomPayment(int currentPage, int rowPerPage,String memberId) {
+		Map<String, Object> paraMap = new HashMap<>();
+		int beginRow = (currentPage-1) * rowPerPage;
+		
+		paraMap.put("beginRow", beginRow);
+		paraMap.put("rowPerPage", rowPerPage);
+		paraMap.put("memberId", memberId);
+		
+		log.trace("☆service☆"+paraMap);
+		
+		List<AccomPayment> accomPaymentList = accomPaymentMapper.selectAccomPayment(paraMap);
+		
+		Map<String, Object> returnMap = new HashMap<>();
+		
+		int lastPage = 0;
+		int totalCount = accomPaymentMapper.selectCountPage(memberId);
+		
+		if(totalCount % rowPerPage !=0) {
+			lastPage += 1;
+		}
+		
+		returnMap.put("accomPaymentList", accomPaymentList);
+		returnMap.put("lastPage", lastPage);
+		returnMap.put("totalCount", totalCount);
+		
+		log.trace("☆service☆"+returnMap);
+		
+		return returnMap;
+	}
+}
