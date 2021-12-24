859a7a121db243debed24ddadb2b8f12912141e1
jihye <jihye@localhost>
2021-12-24 12:06:31 +0900

    [최지혜] chat TABLE 변경과 vo 변경 완료
    chat TABLE 을 닉네임을 외래키로 사용하는게 아닌 ID를 외래키로 사용하는 테이블로 바꿈
    Why? 닉네임을 외래키로 사용하면 회원정보 수정시 닉네임 변경 불가
    - 따라서 ID를 외래키로 사용한 뒤 닉네임 정보를 불러오기로 함
    -> 닉네임 정보를 불러오기 위해서 Chat vo에 Member를 추가함

--- voyage/src/main/java/com/gdu/voyage/vo/Chat.java
+++ voyage/src/main/java/com/gdu/voyage/vo/Chat.java
@@ -5,8 +5,10 @@ import lombok.Data;
 @Data
 public class Chat {
 	private int chatNo;
-	private String toMemberNickName;
-	private String fromMemberNickName;
+	private String toMemberId;
+	private String fromMemberId;
+	private String chatContent;
 	private String createDate;
 	private int chatRead;
+	private Member member; // Member의 닉네임을 조회하기 위
 }
