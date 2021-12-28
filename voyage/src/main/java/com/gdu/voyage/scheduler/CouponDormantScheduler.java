029b4a724a8c6efa98210befb20081f59ee4a382
김보령 <@192.168.219.100>
2021-12-28 14:54:54 +0900

    [김보령]쿠폰 스케쥴러 쿼리문 수정 디버그 코드 수정

--- voyage/src/main/java/com/gdu/voyage/scheduler/CouponDormantScheduler.java
+++ voyage/src/main/java/com/gdu/voyage/scheduler/CouponDormantScheduler.java
@@ -21,16 +21,16 @@ public class CouponDormantScheduler {
 		
 		int row = couponService.updateDormantCoupon();
 		if (row > 0) {
-			log.debug("※※※※※※※※CouponScheduler※※※※※※※※지금까지 총"+row+"개의 쿠폰이 비활성화 되었습니다.");
+			log.debug("※※※※CouponScheduler※※※※"+row+"개의 쿠폰이 비활성화 되었습니다.");
 		}
 	}
 	// 매분1초 마다 실행해서 데드라인을 넘어간 쿠폰을 사용불가 시킨다.
 	@Scheduled(cron = "1 * * * * *")
 	public void modifyDormantMemberCoupon() {
-		
+
 		int row = couponService.updateDormantMemberCoupon();
 		if (row > 0) {
-			log.debug("※※※※※※※※CouponScheduler※※※※※※※※지금까지 총"+row+"개의 쿠폰이 사용불가 되었습니다.");
+			log.debug("※※※※CouponScheduler※※※※"+row+"개의 쿠폰이 사용불가 되었습니다.");
 		}
 	}
 }
