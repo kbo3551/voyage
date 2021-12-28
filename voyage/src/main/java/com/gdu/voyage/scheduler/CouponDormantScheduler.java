9da2fe083229451eb77fd43fae068cad9be58eb6
김보령 <@192.168.219.100>
2021-12-28 17:48:33 +0900

    [김보령]쿠폰 스케쥴러 주기 수정 과도한 콘솔창 메세지로 인해서 임시로 매일 정각에 실행되게 변경

--- voyage/src/main/java/com/gdu/voyage/scheduler/CouponDormantScheduler.java
+++ voyage/src/main/java/com/gdu/voyage/scheduler/CouponDormantScheduler.java
@@ -15,8 +15,8 @@ public class CouponDormantScheduler {
 	@Autowired
 	CouponService couponService;
 	
-	// 매분1초 마다 실행해서 데드라인을 넘어간 쿠폰을 비활성화 시킨다.
-	@Scheduled(cron = "1 * * * * *")
+	// 매분1초 마다 실행해서 데드라인을 넘어간 쿠폰을 비활성화 시킨다.(과도한 콘솔창으로 임시적으로 매일 12에 실행)
+	@Scheduled(cron = "0 0 0 * * *")
 	public void modifyDormantCoupon() {
 		
 		int row = couponService.updateDormantCoupon();
@@ -24,8 +24,8 @@ public class CouponDormantScheduler {
 			log.debug("※※※※CouponScheduler※※※※"+row+"개의 쿠폰이 비활성화 되었습니다.");
 		}
 	}
-	// 매분1초 마다 실행해서 데드라인을 넘어간 쿠폰을 사용불가 시킨다.
-	@Scheduled(cron = "1 * * * * *")
+	// 매분1초 마다 실행해서 데드라인을 넘어간 쿠폰을 사용불가 시킨다.(과도한 콘솔창으로 임시적으로 매일 정각에 실행)
+	@Scheduled(cron = "0 0 0 * * *")
 	public void modifyDormantMemberCoupon() {
 
 		int row = couponService.updateDormantMemberCoupon();
