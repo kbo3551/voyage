39bef5aebb9abaeb81ef84ee2cf323bed8caf946
김보령 <@192.168.219.100>
2022-04-12 00:52:56 +0900

    [김보령]관리자 메인페이지 차트부분 롤백 임시로 쿼리문 작성 CASE 문 사용 null일 경우 다른값 넣어버림 IS NOT NULL도 사용해서 데이터가 있을경우도 원래 작동하던거 처럼 작동함 null값일때 1

--- voyage/src/main/java/com/gdu/voyage/controller/adminPaymentCheckController.java
+++ voyage/src/main/java/com/gdu/voyage/controller/adminPaymentCheckController.java
@@ -20,29 +20,29 @@ public class adminPaymentCheckController {
 	
 	@GetMapping("/admin/adminIndex")
 	public String adminIndexChartData(Model model) {
-//		//수익
-//		long totalPaymentMonth = paymentCheckService.sumPaymentThisMonth();
-//		long totalPaymentYear = paymentCheckService.sumPaymentThisYear();
-//		//결제건수
-//		int totalPersonMonth = paymentCheckService.sumPersonThisMonth();
-//		int totalPersonYear = paymentCheckService.sumPersonThisYear();
-//		//12개월 이내의 각 월 수익
-//		ArrayList<Integer> payList= paymentCheckService.paymentMonthlyYear();
-//		ArrayList<String> calendarList= paymentCheckService.paymentMonthlyYearCalendar();
-//		//총 체험 숙소 결제건수
-//		int totalPersonAccom = paymentCheckService.sumPersonAccom();
-//		int totalPersonActivity = paymentCheckService.sumPersonActivity();
-//		
-//		model.addAttribute("totalPaymentMonth", totalPaymentMonth);
-//		model.addAttribute("totalPaymentYear", totalPaymentYear);
-//		model.addAttribute("totalPersonMonth", totalPersonMonth);
-//		model.addAttribute("totalPersonYear", totalPersonYear);
-//		model.addAttribute("payList",payList);
-//		model.addAttribute("calendarList",calendarList);
-//		model.addAttribute("totalPersonAccom",totalPersonAccom);
-//		model.addAttribute("totalPersonActivity",totalPersonActivity);
-//		
-//		log.debug("★★★[dohun] admin index chart data ★★★" + model);
+		//수익
+		long totalPaymentMonth = paymentCheckService.sumPaymentThisMonth();
+		long totalPaymentYear = paymentCheckService.sumPaymentThisYear();
+		//결제건수
+		int totalPersonMonth = paymentCheckService.sumPersonThisMonth();
+		int totalPersonYear = paymentCheckService.sumPersonThisYear();
+		//12개월 이내의 각 월 수익
+		ArrayList<Integer> payList= paymentCheckService.paymentMonthlyYear();
+		ArrayList<String> calendarList= paymentCheckService.paymentMonthlyYearCalendar();
+		//총 체험 숙소 결제건수
+		int totalPersonAccom = paymentCheckService.sumPersonAccom();
+		int totalPersonActivity = paymentCheckService.sumPersonActivity();
+		
+		model.addAttribute("totalPaymentMonth", totalPaymentMonth);
+		model.addAttribute("totalPaymentYear", totalPaymentYear);
+		model.addAttribute("totalPersonMonth", totalPersonMonth);
+		model.addAttribute("totalPersonYear", totalPersonYear);
+		model.addAttribute("payList",payList);
+		model.addAttribute("calendarList",calendarList);
+		model.addAttribute("totalPersonAccom",totalPersonAccom);
+		model.addAttribute("totalPersonActivity",totalPersonActivity);
+		
+		log.debug("★★★[dohun] admin index chart data ★★★" + model);
 		
 		return "/admin/adminIndex";
 	}
